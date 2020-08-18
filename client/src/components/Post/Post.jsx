1b170a966b58844b47d311b540f7da0a5cdb1c39
Nivo Ratsimbazafy <nivoratsimbazafy@Nivo-Ratsimbazafys-iMac.local>
2020-08-18 12:32:16 -0400

    created components folder

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Product.css';
+import { Link } from 'react-router-dom'
+
+const Post = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Post
