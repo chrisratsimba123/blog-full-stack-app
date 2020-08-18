2d8e230c6f69103655abbb0e6cfd54e0b26f378d
Daynamarie <Dayna.christison@gmail.com>
2020-08-18 13:36:08 -0400

    hello from post-component

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+
 // const productsRoutes = require('./routes/products');
 const db = require('./db/connection')
+
 const PORT = process.env.PORT || 3000
 
 const app = express();
