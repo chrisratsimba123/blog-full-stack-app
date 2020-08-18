8c3fdc56621a5cbaefbc44126bee81138a2a18bf
Daynamarie <Dayna.christison@gmail.com>
2020-08-18 14:21:14 -0400

    added routes, switch and exact path to posts in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
+import Posts from './screens/Posts/Posts'
+
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/" component={Posts}/>
+      </Switch>
       
     </div>
   );
